f7712a36e54054bdd2bd0f096a82b0e54f37ad75
Joshua Navarro <joshua.navarro35@gmail.com>
2022-08-14 03:30:45 -0500

    init configuration to work in TS JS with lua, bugs in eslint, package.json cmp

---
+++ lua/maps.lua
@@ -0,0 +1,29 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Save and Close
+keymap.set('n', '<Leader>w', ':w<CR>')
+keymap.set('n', '<Leader>q', ':q<CR>')
+
+-- New tab
+keymap.set('n', '<Leader>te', ':tabedit<CR>')
+
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+keymap.set('', '<C-h>', '<C-w>h')
+keymap.set('', '<C-j>', '<C-w>j')
+keymap.set('', '<C-k>', '<C-w>k')
+keymap.set('', '<C-l>', '<C-w>l')
+
+-- Resize window
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
---
+++ lua/plugins.lua
@@ -0,0 +1,52 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  use 'L3MON4D3/LuaSnip' -- Snipper
+  use 'hoob3rt/lualine.nvim' -- Statusline
+  use 'onsails/lspkind-nvim' -- vscoder-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'neovim/nvim-lspconfig' -- LSP
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUdate'
+  }
+
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions and more via Lua
+  use 'MunifTanjim/prettier.nvim' -- Prettier plugin form Neovim's built-in LSP client
+  use 'williamboman/mason.nvim' -- Allow to easily manager LSP Servers
+  use 'williamboman/mason-lspconfig.nvim' -- Helper for lspconfig with mason
+
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+
+  use 'akinsho/nvim-bufferline.lua'
+
+  use 'lewis6991/gitsigns.nvim'
+
+  use 'dinhhuy258/git.nvim' -- fork git blame & browser
+  use 'tpope/vim-fugitive'
+
+  use "b0o/schemastore.nvim" -- JSON SchemaStore
+end)
