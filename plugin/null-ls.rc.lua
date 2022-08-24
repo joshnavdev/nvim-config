7ce058fbb9233f8800e5e1fe14d7a8e45fc5c598
Joshua Navarro <joshua.navarro35@gmail.com>
2022-08-24 12:35:34 -0500

    add debug to null-ls

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -2,7 +2,8 @@ local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 
 null_ls.setup({
+  debug = true,
   sources = { null_ls.builtins.diagnostics.eslint.with({
     diagnostics_format = "[#{c}] #{m} (#{s})"
-  }) }
+  }), filetypes = { "javascript", "typescript" } }
 })
